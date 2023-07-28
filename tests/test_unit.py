ee5a59a5098b74ef7a9a6188e4a1927773fae9f6
mraniki <8766259+mraniki@users.noreply.github.com>
2023-07-28 20:07:06 +0200

    🚨

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -12,6 +12,7 @@ from loguru import logger
 from iamlistening import Listener
 from iamlistening.config import settings
 
+
 @pytest.fixture(scope="session", autouse=True)
 def set_test_settings():
     settings.configure(FORCE_ENV_FOR_DYNACONF="testing")
