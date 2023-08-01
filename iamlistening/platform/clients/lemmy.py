3d05f0a91dc71a8418a0955a26a34a48c42ef368
mraniki <8766259+mraniki@users.noreply.github.com>
2023-08-01 19:31:19 +0200

    🧐

--- iamlistening/platform/clients/lemmy.py
+++ iamlistening/platform/clients/lemmy.py
@@ -1,36 +1,36 @@
-"""
-Lemmy  🐭
-"""
-import asyncio
+# """
+# Lemmy  🐭
+# """
+# import asyncio
 
-from loguru import logger
-from pythorhead import Lemmy
+# from loguru import logger
+# from pythorhead import Lemmy
 
-from iamlistening.config import settings
-from iamlistening.platform.chat_manager import ChatManager
+# from iamlistening.config import settings
+# from iamlistening.platform.chat_manager import ChatManager
 
 
-class LemmyHandler(ChatManager):
+# class LemmyHandler(ChatManager):
 
-    def __init__(self):
-        """
-        Initialize the Lemmy handler.
+#     def __init__(self):
+#         """
+#         Initialize the Lemmy handler.
 
  
-        """
-        super().__init__()
+#         """
+#         super().__init__()
 
-    async def start(self):
-        """
-        Start the Lemmy handler.
+#     async def start(self):
+#         """
+#         Start the Lemmy handler.
         
-        """
+#         """
 
-        lemmy = Lemmy(settings.bot_hostname)
-        lemmy.log_in(settings.bot_user,settings.bot_pass)
+#         lemmy = Lemmy(settings.bot_hostname)
+#         lemmy.log_in(settings.bot_user,settings.bot_pass)
 
-        latest_post = lemmy.post.list()[0]["post"]["id"]
+#         latest_post = lemmy.post.list()[0]["post"]["id"]
         
-        await self.handle_message(latest_post)
+#         await self.handle_message(latest_post)
 
 
--- tests/test_unit_lemmy.py
+++ tests/test_unit_lemmy.py
@@ -1,47 +1,47 @@
-"""
-Lemmy Testing
-"""
+# """
+# Lemmy Testing
+# """
 
 
-import asyncio
-from unittest.mock import AsyncMock
+# import asyncio
+# from unittest.mock import AsyncMock
 
-import pytest
+# import pytest
 
-from iamlistening import Listener
-from iamlistening.config import settings
-from iamlistening.platform.chat_manager import ChatManager
-
-
-@pytest.fixture(scope="session", autouse=True)
-def set_test_settings():
-    settings.configure(FORCE_ENV_FOR_DYNACONF="testinglemmy")
-
-@pytest.mark.asyncio
-async def test_fixture():
-    assert settings.VALUE == "On Testing Lemmy"
+# from iamlistening import Listener
+# from iamlistening.config import settings
+# from iamlistening.platform.chat_manager import ChatManager
 
 
-@pytest.fixture(name="listener")
-def listener():
-    return Listener()
+# @pytest.fixture(scope="session", autouse=True)
+# def set_test_settings():
+#     settings.configure(FORCE_ENV_FOR_DYNACONF="testinglemmy")
 
-@pytest.fixture(name="message")
-def message():
-    return "hello"
+# @pytest.mark.asyncio
+# async def test_fixture():
+#     assert settings.VALUE == "On Testing Lemmy"
 
-@pytest.mark.asyncio
-async def test_listener_start(message):
-    handle_iteration_limit = AsyncMock()
-    check_connected = AsyncMock()
-    listener = Listener()
-    await listener.start()
-    await listener.handler.handle_message(message)
-    msg = await listener.handler.get_latest_message()
-    assert listener.handler is not None
-    assert listener.handler.connected is not None
-    assert listener.platform == "lemmy"
-    handle_iteration_limit.assert_awaited
-    check_connected.assert_awaited
-    assert msg == message
+
+# @pytest.fixture(name="listener")
+# def listener():
+#     return Listener()
+
+# @pytest.fixture(name="message")
+# def message():
+#     return "hello"
+
+# @pytest.mark.asyncio
+# async def test_listener_start(message):
+#     handle_iteration_limit = AsyncMock()
+#     check_connected = AsyncMock()
+#     listener = Listener()
+#     await listener.start()
+#     await listener.handler.handle_message(message)
+#     msg = await listener.handler.get_latest_message()
+#     assert listener.handler is not None
+#     assert listener.handler.connected is not None
+#     assert listener.platform == "lemmy"
+#     handle_iteration_limit.assert_awaited
+#     check_connected.assert_awaited
+#     assert msg == message
 
