a558ec7d1e6f54b0fbca3271bfbb8b30db376a33
mraniki <8766259+mraniki@users.noreply.github.com>
2023-08-03 09:43:15 +0200

    📝

--- docs/conf.py
+++ docs/conf.py
@@ -58,42 +58,45 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = "bootstrap"
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
-html_static_path = ["_static"]
-html_css_files = [
-    "custom.css",
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-]
-# html_logo = '_static/logo-full.png'
-html_favicon = '_static/favicon.ico'
-html_copy_source = False
-html_show_sourcelink = False
+
+
 
 html_theme_options = {
-
+    'navbar_site_name': "Site",
+    'navbar_pagenav_name': "Page",
+    'globaltoc_depth': 2,
+    'globaltoc_includehidden': "true",
+    'navbar_class': "navbar",
+    'navbar_fixed_top': "true",
     'source_link_position': "nav",
     'bootswatch_theme': "darkly",
     'bootstrap_version': "3",
-    'globaltoc_depth': 2,
-    'globaltoc_includehidden': "true",
-    'navbar_class': "navbar navbar-inverse",
-    'navbar_pagenav_name': "Sections",
-    'navbar_sidebarrel': False,
-    'navbar_pagenav': False,
-    'navbar_site_name': "Table of Contents",
-    'navbar_links': [
-        ("TalkyTrader", "https://talkytrader.github.io/wiki/",True),
-        ("_menu",  "Plugins",[
-            ("IamListening",  "index"),
-            ("Install",  "IAL_01_install"),
-            ("_divider", ),
-        ]),
-        ("Github", "https://github.com/mraniki/tt", True),
-    ]
+
+
+    # 'navbar_sidebarrel': False,
+    # 'navbar_pagenav': False,
+    # 'navbar_links': [
+    #     ("TalkyTrader", "https://talkytrader.github.io/wiki/",True),
+    #     ("_menu",  "Plugins",[
+    #         ("IamListening",  "index"),
+    #         ("Install",  "IAL_01_install"),
+    #         ("_divider", ),
+    #     ]),
+    #     ("Github", "https://github.com/mraniki/tt", True),
+    # ]
 
 }
 
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_static_path = ["_static"]
+# html_css_files = [
+#     "custom.css",
+# ]
+# html_logo = '_static/logo-full.png'
+html_favicon = '_static/favicon.ico'
+# html_copy_source = False
+# html_show_sourcelink = False
 
 def setup(app):
     app.add_css_file("custom.css")
