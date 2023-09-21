8e015e3d41833b283a6466d8bb68187fd4a4f397
mraniki <mraniki@users.noreply.github.com>
2023-09-21 21:56:41 +0200

    :ambulance: hotfix

--- iamlistening/config.py
+++ iamlistening/config.py
@@ -11,20 +11,14 @@ ROOT = os.path.dirname(__file__)
 # Load the default settings file
 settings = Dynaconf(
     envvar_prefix="TT",
-    # Set the root path of the project
-    # root_path=os.path.dirname(ROOT),
-    # Load the default settings file
+    root_path=os.path.dirname(ROOT),
     settings_files=[
         os.path.join(ROOT, "default_settings.toml"),
-        "talky_settings.toml",
-        "settings.toml",
-        ".secrets.toml",
+        'talky_settings.toml',
+        'settings.toml',
+        '.secrets.toml'
     ],
-    # Load the.env file
     load_dotenv=True,
-    # Set the environments to True
     environments=True,
-    merge_enabled=True,
-    # Set the default environment
     default_env="default",
 )
