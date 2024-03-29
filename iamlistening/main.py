27aaa4affdffeda7d0e6a8cc12114ca83c6e249f
mraniki <8766259+mraniki@users.noreply.github.com>
2024-03-29 08:42:45 +0100

    🔇

--- iamlistening/main.py
+++ iamlistening/main.py
@@ -58,7 +58,7 @@ class Listener:
         self.enabled = settings.iamlistening_enabled or True
         # Create a mapping of library names to client classes
         self.client_classes = self.get_all_client_classes()
-        logger.debug("client_classes available {}", self.client_classes)
+        # logger.debug("client_classes available {}", self.client_classes)
 
         if not self.enabled:
             logger.info("Module is disabled. No clients will be created.")
